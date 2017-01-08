adc254845289067918ea2da5dbd6691991d7e045
Pieter Gheysens <pietergheysens@outlook.com>
2017-01-08 12:48:49 +0100

    adding methods to get access to Eventbrite Access Codes

---
+++ EventbriteNET/AccessCode.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventbriteNET
+{
+    public class AccessCode : EventbriteObject
+    {
+        public AccessCode()
+        {
+            ValidStartDate = new DateTimeTimezoneField();
+            ValidEndDate = new DateTimeTimezoneField();
+        }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("ticket_ids")]
+        public IList<string> TicketIds { get; set; }
+
+        [JsonProperty("quantity_available")]
+        public int QuantityAvailable { get; set; }
+
+        [JsonProperty("quantity_sold")]
+        public int QuantitySold { get; set; }
+
+        [JsonProperty("start_date")]
+        public DateTimeTimezoneField ValidStartDate { get; set; }
+
+        [JsonProperty("end_date")]
+        public DateTimeTimezoneField ValidEndDate { get; set; }
+
+        [JsonProperty("event_id")]
+        public long? EventId { get; set; }
+
+    }
+
+    public class PagedAccessCodes : EventbriteObject
+    {
+        [JsonProperty("pagination")]
+        public Pagination Pagination { get; set; }
+        [JsonProperty("access_codes")]
+        public List<AccessCode> AccessCodes { get; set; }
+    }
+
+}
